72972fbd5e0ce6d5597369fb20a03074d226eda1
justjupit <justjupit@gmail.com>
2021-03-22 11:36:21 +0800

    commit 1

---
+++ app/src/main/java/com/jupiter/application/counter2_030222021/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.jupiter.application.counter2_030222021
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var counter : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun add(v:View){
+       if(v == btn1){
+           counter++
+       }
+        else if(v == btn2){
+           counter+=2
+       }
+        else if(v == btn3){
+           counter = 0
+       }
+        txv.text = counter.toString()
+    }
+
+}
