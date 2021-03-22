cb482efe8cf79b2d71546ddb8168377a25ecc84c
justjupit <justjupit@gmail.com>
2021-03-22 14:43:37 +0800

    commit 1

--- app/src/main/java/com/jupiter/application/counter2_030222021/MainActivity.kt
+++ app/src/main/java/com/jupiter/application/counter2_030222021/MainActivity.kt
@@ -6,13 +6,15 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var counter : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        img.setOnClickListener(this)
 
     }
 
@@ -26,6 +28,12 @@ class MainActivity : AppCompatActivity() {
         else if(v == btn3){
            counter = 0
        }
+        txv.text = counter.toString()
+
+    }
+
+    override fun onClick(v: View?) {
+        counter++
         txv.text = counter.toString()
     }
 
