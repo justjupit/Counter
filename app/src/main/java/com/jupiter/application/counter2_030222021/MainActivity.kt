91fca5e55cba7815a8b384a5cb95ac251c9db135
justjupit <justjupit@gmail.com>
2021-03-29 11:15:37 +0800

    03292021

--- app/src/main/java/com/jupiter/application/counter2_030222021/MainActivity.kt
+++ app/src/main/java/com/jupiter/application/counter2_030222021/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.jupiter.application.counter2_030222021
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener, View.OnTouchListener {
 
     var counter : Int = 0
 
@@ -15,8 +16,37 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         txv.setOnClickListener(this)
         img.setOnClickListener(this)
+        btn4.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                counter = (1..100).random()
+                txv.text = counter.toString()
+            }
+        })
+
+       txv.setOnLongClickListener(object:View.OnLongClickListener{
+            override fun onLongClick(v: View?): Boolean {
+                counter+=2
+                txv.text = counter.toString()
+                return true
+            }
+        })
+
+        txv2.setOnTouchListener(object :View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                //txv2.text="TEST OK"
+                if(event?.action == MotionEvent.ACTION_DOWN){
+                    txv2.text="TEST OK_DOWN(Press)"
+                }
+                else if(event?.action == MotionEvent.ACTION_UP){
+                    txv2.text="TEST OK_UP(Release)"
+                }
+                return true
+            }
+        })
 
     }
+
+
 
     fun add(v:View){
        if(v == btn1){
@@ -39,12 +69,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         else if(v == img){
            counter+=2
        }
-        else if(v == btn4){
+       /* else if(v == btn4){
            counter = (0..100).random()
            txv.text = counter.toString()
 
-       }
+       }*/
         txv.text = counter.toString()
     }
+
+    override fun onLongClick(v: View?): Boolean {
+        counter+=2
+        txv.text = counter.toString()
+        return true
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 
 }
